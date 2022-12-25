a08b5995a87bf0873f934e34066e814619355337
krisnaputra31 <105723874+krisnaputra31@users.noreply.github.com>
2022-12-25 18:48:31 +0700

    try change notes backend to use plugin and posrgressql

--- src/server.js
+++ src/server.js
@@ -1,16 +1,30 @@
-const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+require("dotenv").config();
+const Hapi = require("@hapi/hapi");
+
+const notes = require("./api/notes");
+const NotesService = require("./services/postgres/NotesService");
+const NotesValidator = require("./validator/notes");
 
 const init = async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
-    port: 5000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: true,
     },
   });
 
-  server.route(routes);
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
+    },
+  ]);
 
   await server.start();
   // eslint-disable-next-line no-console
