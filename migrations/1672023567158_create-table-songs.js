eca47a799e43036fe6a711eec8a628809ae2922f
krisnaputra31 <81416312+krisna31@users.noreply.github.com>
2022-12-26 19:16:46 +0700

    add foreign key in songs for album_id

--- migrations/1672023567158_create-table-songs.js
+++ migrations/1672023567158_create-table-songs.js
@@ -33,8 +33,10 @@ exports.up = (pgm) => {
       notNull: false,
     },
   });
+  pgm.addConstraint("songs", "fk_songs.album_id_albums.id", "FOREIGN KEY(album_id) REFERENCES albums(id) ON DELETE CASCADE");
 };
 
 exports.down = (pgm) => {
   pgm.dropTable("songs");
+  pgm.dropConstraint("songs", "fk_songs.album_id_albums.id");
 };
